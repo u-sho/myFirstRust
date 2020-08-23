4283f1d2008cfd93932034aab7a97e00281ca95e
u-sho <shouhei.uechi@gmail.com>
2020-08-23 20:25:56 +0900

    format with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ use std::io;
 // use regex::Regex;
 
 fn main() {
-// let reg = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-// let sys_time = SystemTime::now();
-// println!("Did our date match? {}", reg.is_match("2020-08-23"));
+    // let reg = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    // let sys_time = SystemTime::now();
+    // println!("Did our date match? {}", reg.is_match("2020-08-23"));
 
     println!("Please input your guess.");
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Faild to readline");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Faild to readline");
 
     println!("You guessed: {}", guess);
 }
