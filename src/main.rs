4159815701d2d6d2b4e609112e90bc1326194bb5
u-sho <shouhei.uechi@gmail.com>
2020-08-23 20:25:21 +0900

    use std::io

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use regex::Regex;
+use std::io;
+// use std::time::SystemTime;
+// use regex::Regex;
 
 fn main() {
-    let reg = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
-    println!("Did our date match? {}", reg.is_match("2020-08-23"));
+// let reg = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+// let sys_time = SystemTime::now();
+// println!("Did our date match? {}", reg.is_match("2020-08-23"));
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Faild to readline");
+
+    println!("You guessed: {}", guess);
 }
