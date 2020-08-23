3094302fae68d5f0514ad2c67d6233ec9ae7ed4c
u-sho <shouhei.uechi@gmail.com>
2020-08-23 18:47:38 +0900

    :package: use Regex

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let reg = Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap();
+    println!("Did our date match? {}", reg.is_match("2020-08-23"));
 }
