624c9e9495b788efa0cf1cf0dba0710159f22d05
u-sho <shouhei.uechi@gmail.com>
2020-08-23 21:26:08 +0900

    match expression
    cast: String to u32

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
-// use std::io;
+use std::cmp::Ordering;
+use std::io;
 // use std::time::SystemTime;
 // use regex::Regex;
 
@@ -8,19 +9,27 @@ fn main() {
     // let sys_time = SystemTime::now();
     // println!("Did our date match? {}", reg.is_match("2020-08-23"));
 
-    // println!("Guess the number!");
+    println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
+    // println!("The secret number is: {}", secret_number);
 
-    println!("The secret number is: {}", secret_number);
+    /* input u32 (unsigned 32 bits integer) */
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Faild to readline");
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Please type a non-negative integer less than 2^32!");
 
-    // println!("Please input your guess.");
+    println!("You guessed: {}", guess);
 
-    // let mut guess = String::new();
-
-    // io::stdin()
-    //     .read_line(&mut guess)
-    //     .expect("Faild to readline");
-
-    // println!("You guessed: {}", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
