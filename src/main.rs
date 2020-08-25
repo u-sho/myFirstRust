ad2f1d04fb2f89fe172bef60b79c019f17984fb6
u-sho <shouhei.uechi@gmail.com>
2020-08-25 09:33:05 +0900

    multiple functions

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,52 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
+    guess_game();
+}
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+fn guess_game () {
+    println!("Guess the Secret Number!");
+
+    let secret_num: u8  = get_u8_rand();
 
     loop {
-        /* input u32 (unsigned 32 bits integer) */
-        println!("Please input your guess.");
-        let mut guess = String::new();
+        let guess_num: u8 = get_u8_user_guess();
+        output_game_result(secret_num, guess_num);
+        if guess_num == secret_num {
+            break;
+        }
+    }
+}
+
+fn get_u8_rand() -> u8 {
+    return rand::thread_rng().gen_range(0, 255);
+}
+
+fn get_u8_user_guess() -> u8 {
+    println!("Please input your guess.");
+
+    loop {
+        let mut user_guess = String::new();
         io::stdin()
-            .read_line(&mut guess)
+            .read_line(&mut user_guess)
             .expect("Faild to readline");
-        let guess: u32 = match guess.trim().parse() {
+
+        let user_guess: u8 = match user_guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("Please type a integer from 1 to 100");
+                println!("Please input a integer from 0 to 255.");
                 continue;
             }
         };
 
-        // println!("You guessed: {}", guess);
+        break user_guess;
+    }
+}
 
-        /* output result */
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+fn output_game_result(secret_num: u8, guess_num: u8) {
+    match guess_num.cmp(&secret_num) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!")
     }
 }
